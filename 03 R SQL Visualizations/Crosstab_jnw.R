5b8e0c6023819592f317cd1277552528f9666e48
jennawebb117 <jennawebb@utexas.edu>
2015-10-25 19:03:40 -0500

    more updating

--- 03 R SQL Visualizations/Crosstab_jnw.R
+++ 03 R SQL Visualizations/Crosstab_jnw.R
@@ -7,30 +7,30 @@ KPI_Low_value = 10
 KPI_Medium_value = 100
 
 df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", '129.152.144.84:5001/rest/native/?query=
-"select DISEASE, COUNTY, avg_count, kpi as ratio, 
+"select SEX, County, kpi, 
 case
 when kpi <= "p1" then \\\'Very Low\\\'
 when kpi <= "p2" then \\\'Low\\\'
 when kpi <= "p3" then \\\'Medium\\\'
 else \\\'High\\\'
 end kpi
-from (select DISEASE, COUNTY, COUNT, 
-   avg(COUNT) avg_count, 
-   COUNT kpi
-   from INFECTIOUS_DISEASES
-   group by DISEASE, COUNTY)
-order by COUNTY;"
-')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_jnw653', PASS='orcl_jnw653', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', p1=KPI_Very_Low_value, p2=KPI_Low_value, p3=KPI_Medium_value), verbose = TRUE))) 
+from (select SEX, County, COUNT, 
+avg(Count) kpi
+from INFECTIOUS_DISEASES
+group by SEX, County, COUNT)
+order by County;"
+')),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_jnw653', PASS='orcl_jnw653', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', p1=KPI_Very_Low_value, p2=KPI_Low_value, p3=KPI_Medium_value), verbose = TRUE)))
+
 
 
 ggplot() + 
   coord_cartesian() + 
   scale_x_discrete() +
   scale_y_discrete() +
-  labs(title='Titlellellelel') +
-  labs(x=paste("DISEASE"), y=paste("COUNTY")) +
+  labs(title='Number of Diseases per Sex per County') +
+  labs(x=paste("SEX"), y=paste("County")) +
   layer(data=df, 
-        mapping=aes(x=DISEASE, y=COUNTY, label=COUNT), 
+        mapping=aes(x=SEX, y=COUNTY, label=COUNTY), #label is wrong here
         stat="identity", 
         stat_params=list(), 
         geom="text",
@@ -38,26 +38,11 @@ ggplot() +
         position=position_identity()
   ) +
   layer(data=df, 
-        mapping=aes(x=DISEASE, y=COUNTY, label=COUNT), 
-        stat="identity", 
-        stat_params=list(), 
-        geom="text",
-        geom_params=list(colour="black", vjust=2), 
-        position=position_identity()
-  ) +
-  layer(data=df, 
-        mapping=aes(x=DISEASE, y=COUNTY, label=round(RATIO, 2)), 
+        mapping=aes(x=SEX, y=COUNTY, label=COUNTY), #label is wrong here
         stat="identity", 
         stat_params=list(), 
         geom="text",
         geom_params=list(colour="black", vjust=4), 
         position=position_identity()
-  ) +
-  layer(data=df, 
-        mapping=aes(x=DISEASE, y=COUNTY, fill=KPI), 
-        stat="identity", 
-        stat_params=list(), 
-        geom="tile",
-        geom_params=list(alpha=0.50), 
-        position=position_identity()
-  )
+  ) 
+
