1941df51c382ecb364981666ca784ac9a9fcacbe
jennawebb117 <jennawebb@utexas.edu>
2015-11-05 17:24:33 -0600

    updated with R code

--- 01 Data/InfectiousDiseases.R
+++ 01 Data/InfectiousDiseases.R
@@ -2,5 +2,4 @@ require("jsonlite")
 require("RCurl")
 require("ggplot2")
 
-# Change the USER and PASS below to be your UTEid
-diseases <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from Infectious_Diseases"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_hys82', PASS='orcl_hys82', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+diseases <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from Infectious_Diseases"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_jnw653', PASS='orcl_jnw653', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
