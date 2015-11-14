44ff69d4180e121c958e9d15aa2855407da04f32
Happy Situ <happysitu@utexas.edu>
2015-11-13 22:26:44 -0600

    half the diseases with R and png

--- 03 R SQL Visualizations/BarchartWithRef.R
+++ 03 R SQL Visualizations/BarchartWithRef.R
@@ -7,7 +7,7 @@ from Infectious_Diseases
 where DISEASE = (\'Chlamydia\') and (SEX=(\'Male\') OR SEX=(\'Female\') )
 group by YEAR, DISEASE)
 order by DISEASE;"
-')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_hys82', PASS='orcl_hys82', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', p1=KPI_Low_Max_value, p2=KPI_Medium_Max_value), verbose = TRUE))); View(df)
+')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_hys82', PASS='orcl_hys82', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', p1=KPI_Low_Max_value, p2=KPI_Medium_Max_value), verbose = TRUE))); 
 
 dfMALE <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
 "select YEAR, DISEASE, sum_COUNT, sum(sum_COUNT) 
@@ -18,20 +18,20 @@ from Infectious_Diseases
 where DISEASE = (\'Chlamydia\') and SEX=(\'Male\')
 group by YEAR, DISEASE)
 order by DISEASE;"
-')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_hys82', PASS='orcl_hys82', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', p1=KPI_Low_Max_value, p2=KPI_Medium_Max_value), verbose = TRUE))); View(dfMALE)
+')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_hys82', PASS='orcl_hys82', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', p1=KPI_Low_Max_value, p2=KPI_Medium_Max_value), verbose = TRUE)));
 
 ggplot() + 
   coord_cartesian() + 
   scale_x_continuous() +
   scale_y_continuous() +
-  labs(title='CHLAMYDIA AVERAGE_COUNT, WINDOW_AVG_COUNT, ') +
-  labs(x=paste("YEAR"), y=paste("sum_COUNT")) +
+  labs(title='CHLAMYDIA AVERAGE_COUNT, WINDOW_AVG_COUNT') +
+  labs(x=paste("YEAR"), y=paste("COUNT OVER ALL COUTIES PER YEAR")) +
   layer(data=df, 
         mapping=aes(x=YEAR, y=SUM_COUNT), 
         stat="identity", 
         stat_params=list(), 
         geom="bar",
-        geom_params=list(color="RED"), 
+        geom_params=list(fill="RED"), 
         position=position_identity()
   ) + coord_flip() +
   layer(data=dfMALE, 
@@ -39,11 +39,19 @@ ggplot() +
         stat="identity", 
         stat_params=list(), 
         geom="bar",
-        geom_params=list(color="BLUE"), 
+        geom_params=list(fill="BLUE"), 
         position=position_identity()
   ) + coord_flip() +
   layer(data=df, 
         mapping=aes(yintercept = WINDOW_AVG_COUNT/14), 
         geom="hline",
         geom_params=list(colour="BLACK")
-  ) 
+  ) +
+  layer(data=df, 
+        mapping=aes(x=2000, y=round(WINDOW_AVG_COUNT/14), label=round(WINDOW_AVG_COUNT/14)), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="text",
+        geom_params=list(colour="black", hjust=-.1), 
+        position=position_identity()
+  )
---
+++ 03 R SQL Visualizations/Coccidioidomycosis.R
@@ -0,0 +1,57 @@
+df <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
+"select YEAR, DISEASE, sum_COUNT, sum(sum_COUNT) 
+                                                OVER (PARTITION BY DISEASE) as window_avg_COUNT
+                                                from 
+                                                (select YEAR, DISEASE, sum(COUNT) as sum_COUNT
+                                                from Infectious_Diseases
+                                                where DISEASE = (\'Coccidioidomycosis\') and (SEX=(\'Male\') OR SEX=(\'Female\') )
+                                                group by YEAR, DISEASE)
+                                                order by DISEASE;"
+                                                ')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_hys82', PASS='orcl_hys82', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', p1=KPI_Low_Max_value, p2=KPI_Medium_Max_value), verbose = TRUE))); 
+
+dfMALE <- data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
+                                                    "select YEAR, DISEASE, sum_COUNT, sum(sum_COUNT) 
+                                                    OVER (PARTITION BY DISEASE) as window_avg_COUNT
+                                                    from 
+                                                    (select YEAR, DISEASE, sum(COUNT) as sum_COUNT
+                                                    from Infectious_Diseases
+                                                    where DISEASE = (\'Coccidioidomycosis\') and SEX=(\'Male\')
+                                                    group by YEAR, DISEASE)
+                                                    order by DISEASE;"
+                                                    ')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_hys82', PASS='orcl_hys82', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON', p1=KPI_Low_Max_value, p2=KPI_Medium_Max_value), verbose = TRUE)));
+
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_continuous() +
+  scale_y_continuous() +
+  labs(title='COCCIDIOIDOMYCOSIS AVERAGE_COUNT, WINDOW_AVG_COUNT') +
+  labs(x=paste("YEAR"), y=paste("COUNT OVER ALL COUTIES PER YEAR")) +
+  layer(data=df, 
+        mapping=aes(x=YEAR, y=SUM_COUNT), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="bar",
+        geom_params=list(fill="RED"), 
+        position=position_identity()
+  ) + coord_flip() +
+  layer(data=dfMALE, 
+        mapping=aes(x=YEAR, y=SUM_COUNT), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="bar",
+        geom_params=list(fill="BLUE"), 
+        position=position_identity()
+  ) + coord_flip() +
+  layer(data=df, 
+        mapping=aes(yintercept = WINDOW_AVG_COUNT/14), 
+        geom="hline",
+        geom_params=list(colour="BLACK")
+  ) +
+  layer(data=df, 
+        mapping=aes(x=2000, y=round(WINDOW_AVG_COUNT/14), label=round(WINDOW_AVG_COUNT/14)), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="text",
+        geom_params=list(colour="black", hjust=-.1), 
+        position=position_identity()
+  )
